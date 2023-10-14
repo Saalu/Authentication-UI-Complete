d8f85c3c80265c6cba9a774ca0855130fd0aaad0
Saalu Issaka <saaluissaka@gmail.com>
2023-10-14 19:57:35 +0000

    Posts ui design

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,17 +1,10 @@
-
+import Dashboard from '../components/Dashboard';
 const Account = () => {
   return (
-    <div className='h-screen bg-gray-100 flex justify-center items-center'>
-      <h2 className="m-5 text-[28px]">Accounts Route</h2>
-      {/* <p>Lorem ipsum, dolor sit amet consectetur 
-        adipisicing elit. Perferendis adipisci vel
-         autem accusantium nihil dicta nesciunt rerum
-          veritatis corrupti commodi eaque expedita 
-           perspiciatis ipsum non est odit beatae aperiam,
-            ad pariatur nulla asperiores molestias.
-             Quis deserunt quaerat eos, nam hic repudiandae 
-             expedita officia veritatis at error quasi amet ex!
-             </p> */}
+    <div className='h-screen bg-gray-100 flex justify-start items-center'>
+      {/* <h2 className="m-5 text-[28px]">Accounts Route</h2> */}
+     
+      <Dashboard/>
     </div>
   )
 }
